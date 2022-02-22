555d9dcc6359e74f190fe9b30f087fea6e212cb8
aditepDev <aditep.dev@gmail.com>
2022-02-22 23:06:02 +0700

    add, #1 member repository

--- shop/src/main/java/com/demo/shop/user/repository/MemberRepository.java
+++ shop/src/main/java/com/demo/shop/user/repository/MemberRepository.java
@@ -1,2 +1,7 @@
-package com.demo.shop.user.repository;public interface MemberRepository {
+package com.demo.shop.user.repository;
+
+import com.demo.shop.user.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
